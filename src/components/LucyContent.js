32264958e1be2b50e857cac2b153d1344bb23178
Juliet Jacobson <julietanna@users.noreply.github.com>
2022-05-09 10:37:23 -0400

    first draft text

--- src/components/LucyContent.js
+++ src/components/LucyContent.js
@@ -15,55 +15,55 @@ const images = [
 
   const captions = [
     {
-      label: '0 1st image',
-      text: '<p>illus_01 Looking down on East Newburgh, urban planners saw blighted buildings, obsolete land-use patterns, and an outdated street grid. The city\'s population, as well as its tax base, was in decline. The planners\' solution to this cluster of problems was to start over from scratch.</p>',
+      label: '0 1st image - house exterior, stars visible',
+      text: '<p>Enslaved at the Wells&rsquo; house.</p><p>Scroll down to begin</p>',
     },
     {
-      label: '1 second image',
-      text: '<p>illus_02 This was the logic of urban renewal:</p><p>Demolition of existing homes and shops was the first step toward transforming a blighted area.</p>'
+      label: '1 2nd image, house exterior, sky lightens',
+      text: '<p>In colonial New England, work days for enslaved people begin long before daybreak—long before their enslavers get up to begin their own work days.</p>',
     },
     {
-      label: '2 third',
-      text: '<p><p>illus_03 Once cleared, small lots could be repackaged into larger parcels for sale to private developers.</p>'
+      label: '2 3rd image - ',
+      text: '<p>While Ebenezer and Abigail Wells continue to sleep in their comfortable bed near the front of the house, Lucy arises from her tick-filled mattress in the garret and lights a candle so that she can see to dress for the day.</p>'
     },
     {
-      label: '3 fourth - the labels have to be unique',
-      text: '<p>Still illus_03 Looking down on East Newburgh, urban planners saw blighted buildings, obsolete land-use patterns, and an outdated street grid. The city\'s population, as well as its tax base, was in decline. The planners\' solution to this cluster of problems was to start over from scratch.</p>',
+      label: '3 4th image - ',
+      text: '<p>Caesar, sleeping on another garret mattress, also awakens and dresses. Lucy and Caesar slip quietly down the stairs to the kitchen. </p>',
     },
     {
-      label: '4 five',
-      text: '<p>frame 4 This was the logic of urban renewal:</p><p>Demolition of existing homes and shops was the first step toward transforming a blighted area.</p>'
+      label: '4 5th image - ',
+      text: '<p>There Caesar builds a new fire on the embers remaining from the night before, while Lucy begins to prepare breakfast for the Wells. She and Caesar will eat later.</p>'
     },
     {
-      label: '5 six',
-      text: '<p>frame 5 Once cleared, small lots could be repackaged into larger parcels for sale to private developers.</p>'
+      label: '5 6th image - ',
+      text: '<p>Lucy cracks several eggs and places a pot of baked beans at the front of the fire to reheat. She also hangs a pot of water over the fire to boil for tea and puts a jug of apple cider on the table. Finally, she sets out plates, cups, and tableware for the Wells.</p>'
     },
     {
-      label: '6 seven',
-      text: '<p>frame 6 Once cleared, small lots could be repackaged into larger parcels for sale to private developers.</p>'
+      label: '6 7th image - ',
+      text: '<p>Having renewed the fire, Caesar goes outside with a bucket of cracked corn to feed the chickens.</p>'
     },
     {
-      label: '7 eight',
-      text: '<p>frame 7 Once cleared, small lots could be repackaged into larger parcels for sale to private developers.</p>'
+      label: '7 8th image - Interlude',
+      text: '<p>Note: note to viewers of this mockup: the narrative continues to follow the activities of Lucy, Cesar and the Wells through their day. For this mockup, we&apos;ll jump ahead to the end of the day&hellip;</p>'
     },
     {
-      label: '8 nine',
-      text: '<p>frame 8 Once cleared, small lots could be repackaged into larger parcels for sale to private developers.</p>'
+      label: '8 9th image - ',
+      text: '<p>Night has fallen, and Lucy continues to work, cleaning up the dinner dishes by candlelight.  Caesar stokes and banks the fire for the night and then prepares to go out, this time on an errand of his own.</p>'
     },
     {
-      label: '9 ten',
-      text: '<p>frame 9 Once cleared, small lots could be repackaged into larger parcels for sale to private developers.</p>'
+      label: '9 10th image - ',
+      text: '<p>He plans to meet one of his friends for a leisurely walk—she is waiting for him in the yard behind the Wells house.</p>'
     },
     {
-      label: '10 eleven',
-      text: '<p>frame 10 Once cleared, small lots could be repackaged into larger parcels for sale to private developers.</p>'
+      label: '10 11th image - ',
+      text: '<p>She is a friend of Lucy&rsquo;s, too, and Lucy gives Caesar a package for her, asking him to relay a message proposing that the two young women meet sometime the following day.</p>'
     },
     {
-      label: '11 twelve',
-      text: '<p>frame 11 Once cleared, small lots could be repackaged into larger parcels for sale to private developers.</p>'
+      label: '11 12th image - ',
+      text: '<p>Enslaved people have odds and ends of free time nearly every day, although they must fit them into a fairly rigid schedule of tasks determined by the season. Because it is now autumn, the schedule is especially demanding: harvesting vegetables and grains, preserving fruits, butchering animals and preserving their meat, and gathering and chopping wood for the winter’s fires. Most of these tasks will employ enslaved people and their enslavers working together.</p>'
     },
     {
-      label: '12 Thirteen',
+      label: '12 13th image - ',
       text: '<p>frame 12 Once cleared, small lots could be repackaged into larger parcels for sale to private developers.</p>'
     },
     {
