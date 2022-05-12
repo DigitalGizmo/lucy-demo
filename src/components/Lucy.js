b0a1211f85ccfd649ded8a3ef3b2221f19cefcc0
Don Button <donpublic@digitalgizmo.com>
2022-05-12 11:59:42 -0400

    add dynamic more texts

--- src/components/Lucy.js
+++ src/components/Lucy.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'; // , { useEffect }
 import {motion, AnimatePresence, useViewportScroll, useTransform } from 'framer-motion'; // , useAnimation
 import { InView } from "react-intersection-observer"; // useInView, 
-import { images, captions, audio } from './LucyContent';
+import { images, captions, audio, mores } from './LucyContent';
 
 const Lucy = () => {
   const NUM_CAPTIONS = 14;
@@ -9,7 +9,8 @@ const Lucy = () => {
   const [isPinned, setIsPinned] = useState(true);
   const { scrollYProgress } = useViewportScroll(); // scrollY, 
   const [isZoomable, setIsZoomable] = useState(true);
-  const [imageIndex, setImageIndex] = useState(99);
+  const [moreIndex, setMoreIndex] = useState(0);
+  // const [moreIndex, setMoreIndexTracker] = useState(0);
   // const scrollAction = useTransform(
   //   scrollY, [0, 100], [0, 100]
   // );
@@ -18,10 +19,10 @@ const Lucy = () => {
     // console.log('got to play audio');
     bgAudio.play();
   }
-  const pauseAudio = () => {
-    // console.log('got to pause');
-    bgAudio.pause();
-  }  
+  // const pauseAudio = () => {
+  //   // console.log('got to pause');
+  //   bgAudio.pause();
+  // }  
   const fudgFactor = .04;
 
   const getThresholds = () => {
@@ -70,47 +71,69 @@ const Lucy = () => {
     [0, 360, 360, 0, 0]
   )  
 
+  // useEffect(() => {
+  //   setMoreIndexTracker(imageIndex);
+  // },[imageIndex])
+
   useEffect(() => {
     scrollYProgress.onChange((value) => {
         if (value < thresholds[1]) {
-          // if (imageIndex != 0) {
-            // setImageIndex(0);
+          // console.log('imageIndex before if: ' + imageIndex);
+          if (moreIndex !== 0) {
+            setMoreIndex(0);
+            // console.log('imageIndex after set: ' + imageIndex);
             // pauseAudio()
             setImageName(images[0])
-          // }
+          }
         } else if (value >= thresholds[1] && value < thresholds[2]) {
-          playAudio()
-          setImageName(images[1])
+          // console.log('imageIndex-1 before if: ' + imageIndex);
+          if (moreIndex !== 1) {
+            setMoreIndex(1);
+            // console.log('imageIndex-1 after set: ' + imageIndex);
+            // pauseAudio()
+            playAudio()
+            setImageName(images[1])
+          }
         } else if (value >= thresholds[2] && value < thresholds[3]) { // hold
+          setMoreIndex(2);
           setImageName(images[2])
         } else if (value >= thresholds[3] && value < thresholds[5]) {
           // pauseAudio()
           setIsZoomable(true)
+          setMoreIndex(3);
           setImageName(images[3]) // zoom into downstairs
         } else if (value >= thresholds[5] && value < thresholds[6]) {
           setIsZoomable(false)
+          setMoreIndex(5);
           setImageName(images[4]) // Lucy at hearth
         } else if (value >= thresholds[6] && value < thresholds[7]) {
+          setMoreIndex(6);
           setImageName(images[5])
         } else if (value >= thresholds[7] && value < thresholds[8]) {
+          setMoreIndex(7);
           setImageName(images[6])
         } else if (value >= thresholds[8] && value < thresholds[9]) {
+          setMoreIndex(8);
           setImageName(images[7])
         } else if (value >= thresholds[9] && value < thresholds[10]) {
+          setMoreIndex(9);
           setImageName(images[8])
         } else if (value >= thresholds[10] && value < thresholds[11]) {
+          setMoreIndex(10);
           setImageName(images[9])
         } else if (value >= thresholds[11] && value < thresholds[12]) {
+          setMoreIndex(11);
           setImageName(images[10])
         } else if (value >= thresholds[12] && value < thresholds[13]) {
           setIsPinned(true)
+          setMoreIndex(12);
           setImageName(images[11]) // stepping out, last image
           // console.log('value between 12 and 13: ' + value)
         } else if (value >= thresholds[13] ) { // && value < thresholds[14]
           setIsPinned(false)
         }
     })
-}, [scrollYProgress, thresholds])
+}, [scrollYProgress, thresholds, moreIndex])
 
   const dissolve = {
     initial: { 
@@ -155,51 +178,51 @@ const Lucy = () => {
           <a href="../index.html?mocknum=21">MORE</a>
         </span>
 
-        <article>
-          <h2>related to this</h2>
-          <p>This is the contextual material that related precisely to the image currently being shown. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <article
+          dangerouslySetInnerHTML={{ __html: mores[moreIndex].text }}
+        >
         </article>
       </div>
 
 
-        <div className={`dummy ${isPinned ? "image-panel-outer-div" : "hidden"} `}>
+      <div className={`dummy ${isPinned ? "image-panel-outer-div" : "hidden"} `}>
 
-          <AnimatePresence initial={false}>
-              <motion.div
-                key={imageName}
-                initial="initial"
-                animate="animate"
-                exit="exit"
-                variants = { dissolve }
-                style={{ 
-                  x: isZoomable ? sX : 0, 
-                  scale: isZoomable ? sScale : 1
-                }}
-                //  x: scrollAction    , scale: vScale         
-              >
-                <img 
-                  alt={imageName}
-                  src={`https://dev.digitalgizmo.com/lucy-assets/images/${imageName}`}
-                />
-              </motion.div>            
+        <AnimatePresence initial={false}>
+            <motion.div
+              key={imageName}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              variants = { dissolve }
+              style={{ 
+                x: isZoomable ? sX : 0, 
+                scale: isZoomable ? sScale : 1
+              }}
+              //  x: scrollAction    , scale: vScale         
+            >
+              <img 
+                alt={imageName}
+                src={`https://dev.digitalgizmo.com/lucy-assets/images/${imageName}`}
+              />
+            </motion.div>            
 
-          </AnimatePresence>
+        </AnimatePresence>
 
-        </div>
-      </div>{/*  /image-panel */}
-
-      <div className="caption-panel">
-        {/* custom first caption */}
-        { captionDissolves }
       </div>
-      <div className="don-last-page">
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p><p> quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu</p><p> fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p><p> ut labore et dolore magna aliqua. Ut enim </p><p>ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute </p><p>irure dolor in reprehenderit in voluptate velit esse</p><p> cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p><p> deserunt mollit anim id est laborum.
-        </p>
-      </div>
+    </div>{/*  /image-panel */}
+
+    <div className="caption-panel">
+      {/* custom first caption */}
+      { captionDissolves }
+    </div>
+    <div className="don-last-page">
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p><p> quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu</p><p> fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p><p> ut labore et dolore magna aliqua. Ut enim </p><p>ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute </p><p>irure dolor in reprehenderit in voluptate velit esse</p><p> cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p><p> deserunt mollit anim id est laborum.
+      </p>
+    </div>
 
     </section>
   );
--- src/components/LucyContent.js
+++ src/components/LucyContent.js
@@ -74,6 +74,61 @@ const captions = [
   },
 ];
 
+const mores = [
+  {
+    label: '0 1st image - house exterior, stars visible',
+    text: '<h2>Crickets</h2><p>Nighttime -- crickets! See more about <a href="https://en.wikipedia.org/wiki/Cricket_(insect)" target="_blank">crickets</a>  </p>',
+  },
+  {
+    label: '1 2nd image, house exterior, sky lightens',
+    text: '<h2>Some Title</h2><p>In colonial New England, work days for enslaved people begin long before daybreak—long before their enslavers get up to begin their own work days.</p>',
+  },
+  {
+    label: '2 3rd image - ',
+    text: '<h2>Title 2</h2><p>While Ebenezer and Abigail Wells continue to sleep in their comfortable bed near the front of the house, Lucy arises from her tick-filled mattress in the garret and lights a candle so that she can see to dress for the day.</p>'
+  },
+  {
+    label: '3 4th image - ',
+    text: '<h2>Some Title 3</h2><p>Caesar, sleeping on another garret mattress, also awakens and dresses. Lucy and Caesar slip quietly down the stairs to the kitchen. </p>',
+  },
+  {
+    label: '4 5th image - ',
+    text: '<h2>Some Title 4</h2><p>There Caesar builds a new fire on the embers remaining from the night before, while Lucy begins to prepare breakfast for the Wells. She and Caesar will eat later.</p>'
+  },
+  {
+    label: '5 6th image - ',
+    text: '<h2>Some Title 5</h2><p>Lucy cracks several eggs and places a pot of baked beans at the front of the fire to reheat. She also hangs a pot of water over the fire to boil for tea and puts a jug of apple cider on the table. Finally, she sets out plates, cups, and tableware for the Wells.</p>'
+  },
+  {
+    label: '6 7th image - ',
+    text: '<h2>Some Title 6</h2><p>Having renewed the fire, Caesar goes outside with a bucket of cracked corn to feed the chickens.</p>'
+  },
+  {
+    label: '7 8th image - Interlude',
+    text: '<h2>Some Title 7</h2><p>Note: note to viewers of this mockup: the narrative continues to follow the activities of Lucy, Cesar and the Wells through their day. For this mockup, we&apos;ll jump ahead to the end of the day&hellip;</p>'
+  },
+  {
+    label: '8 9th image - moon rise animation',
+    text: '<h2>Some Title</h2><p>Night has fallen, and Lucy continues to work, cleaning up the dinner dishes by candlelight. Caesar has returned from errands he was on for Ebenezer earlier in the day.</p>'
+  },
+  {
+    label: '9 10th image - friend in yard',
+    text: '<h2>Some Title</h2><p>Caesar stokes and banks the fire for the night and then prepares to go out, this time on an errand of his own. He plans to meet one of his friends for a leisurely walk—she is waiting for him in the yard behind the Wells house.</p>'
+  },
+  {
+    label: '10 11th image - lucy hands cesar package',
+    text: '<h2>Some Title</h2><p>She is a friend of Lucy&apos;s, too, and Lucy gives Caesar a package for her, asking him to relay a message proposing that the two young women meet sometime the following day.</p>'
+  },
+  {
+    label: '11 12th image - cesar and friend leaving',
+    text: '<h2>Some Title 4</h2><p>Cesar and Lucy are eager to hear news from other households and share their own updates.</p>'
+  },
+  {
+    label: '12 13th image - close up of lucy ',
+    text: '<h2>Some Title 4</h2><p>Lucy is anxious, apprehensive, and excited. She worries that Wells&apos; will discover that Caesar has been out.</p>'
+  },
+];
 export {images}
 export {audio}
 export {captions}
+export {mores}
